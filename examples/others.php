c007fa6101f321b4337b1e1027e4518c5430e8f6
radiegtya <radiegtya@yahoo.co.id>
2015-05-18 16:22:21 +0700

    others example

---
+++ examples/others.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once '../vendor/autoload.php';
+require_once '../src/api/Others.php';
+require_once '../src/api/Rapid.php';
+require_once '../src/api/Response.php';
+
+use Rapid\Api\Rapid;
+use Rapid\Api\Others;
+
+Rapid::init("5b107695161b40780be8e1d3e4c17da4c8820244");
+
+//GET ALL
+echo "<h3>GET ALL Example</h3>";
+$response = Others::getAll();
+print_r($response->headers());
+
+echo "<br/>";
+
+echo $response->paginate();
+
+foreach ($response->data() as $response) {
+    echo "<hr/>";
+    echo $response->_id . "<br/>";
+    echo $response->title . "<br/>";
+    echo $response->image . "<br/>";
+    echo $response->short . "<br/>";
+    echo $response->description . "<br/>";
+    echo "<hr/>";
+}
+
+//GET
+echo "<h3>GET Example with id zps2qaHyCwtXKqicc</h3>";
+$id = "qgRqmZS2DkrtCKMhj";
+$response = Others::get("other-2");
+echo $response->data()->title;
+
+////LOGIN
+//echo "<h3>LOGIN Example</h3>";
+//$response = Rapid::login('tiyo-news-admin', 'admin');
+//print_r($response);
+
+////LOGOUT
+//echo "<h3>LOGOUT Example</h3>";
+//$response = Rapid::logout();
+//print_r($response);
+
+////POST
+//echo "<h3>POST Example </h3>";
+//$body = [
+//    'title' => 'My Fourth Post',
+//    'slug' => 'my-fourth-post'
+//];
+//$response = Others::post($body);
+//print_r($response->message());
+
+////PUT
+//echo "<h3>PUT Example</h3>";
+//$id = "zps2qaHyCwtXKqicc";
+//$body = [
+//    'title' => 'My Fifth Post',
+//    'slug' => 'my-fifth-post'
+//];
+//$response = Others::put($id, $body);
+//print_r($response);
+
+////DELETE
+//echo "<h3>DELETE Example</h3>";
+//$id = "zps2qaHyCwtXKqicc";
+//$response = Others::delete($id);
+//print_r($response);
+
+////DELETE ALL
+//echo "<h3>DELETE ALL Example</h3>";
+//$response = Others::deleteAll();
+//print_r($response);
+
+
+
