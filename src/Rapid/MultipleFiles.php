f1cb5bb47a515ec7ff92d713537daf46ac1a6fc3
radiegtya <radiegtya@yahoo.co.id>
2015-10-11 21:32:31 +0700

    adding multiplefiles for catalogs

--- src/Rapid/MultipleFiles.php
+++ src/Rapid/MultipleFiles.php
@@ -9,10 +9,16 @@ use Unirest\Request;
 class MultipleFiles {
 
     private static $route = 'multipleFiles';
-    private static $belongsToRoute = 'files';
+    private static $belongsToRouteFiles = 'files';
+    private static $belongsToRouteCatalogs = 'catalogs';
 
     public static function getAllByFile($fileIdOrSlug, $params = null, $headers = null) {
-        $response = Request::get(Rapid::getUrl(self::$belongsToRoute . '/' . $fileIdOrSlug . '/'. self::$route), $headers, $params);
+        $response = Request::get(Rapid::getUrl(self::$belongsToRouteFiles . '/' . $fileIdOrSlug . '/'. self::$route), $headers, $params);
+        return Response::make($response);
+    }    
+    
+    public static function getAllByCatalog($catalogIdOrSlug, $params = null, $headers = null) {
+        $response = Request::get(Rapid::getUrl(self::$belongsToRouteCatalogs . '/' . $catalogIdOrSlug . '/'. self::$route), $headers, $params);
         return Response::make($response);
     }    
 
